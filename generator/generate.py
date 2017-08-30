5e9e333533b6424f223c4ce99cb1cb7503ddf48c
RadialApps <radialapps@gmail.com>
2017-08-30 18:46:52 +0530

    Improvement to generator

--- generator/generate.py
+++ generator/generate.py
@@ -1,65 +1,107 @@
 import pandas as pd
 import re
 
+# Read our data into a pandas dataframe
 print('Reading data')
-df=pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
 
+# Read the template file
 print('Reading template')
 with open('template.html') as f:
-    lines = f.read()
+    template = f.read()
 
+# Just to be safe
 rowno = 0
 
+# Replace any unsafe characters for http links
+# TODO: expand this to replace other special characters
 def replace_special(input):
     return input.replace('&', '&amp;')
 
-def square_replace(match):
+# Preprocessing
+def preprocess(match):
     match = match.group()
+    
+    # Get the string to be processed
     com = match.strip("[]")
     if com[:1]!="#":
         return match
-    for x in re.findall(regexcaret, match):
-        if df[x][rowno] == 1:
-            if len(re.findall(regexdollar, match)) > 0:
-                print(x, ' == true -- ', re.findall(regexdollar, match)[0])
-                return re.findall(regexdollar, match)[0]
-        elif df[x][rowno] == 0:
-            if len(re.findall(regexnotdollar, match)) > 0:
-                print(x, ' == false -- ', re.findall(regexnotdollar, match)[0])
-                return re.findall(regexnotdollar, match)[0]
 
-    print(match, " -- no match")
+    # Look for conditions
+    for col in re.findall(regexcaret, com):
+        # Check if such a column exists
+        if not col in df.columns:
+            continue
+        
+        if df[col][rowno] == 1:
+            # Answer if true
+            if len(re.findall(regexdollar, com)) > 0:
+                print(col, ' == true -- ', re.findall(regexdollar, com)[0])
+                return re.findall(regexdollar, com)[0]
+
+        elif df[col][rowno] == 0:
+            # Answer if false
+            if len(re.findall(regexnotdollar, com)) > 0:
+                print(col, ' == false -- ', re.findall(regexnotdollar, com)[0])
+                return re.findall(regexnotdollar, com)[0]
+
+    print(com, " -- no match")
     return ""
 
-def curl_replace(match):
+# Actual compilation
+def compile(match):
     match = match.group()
+    
+    # Get the string to be processed
     col = match.strip("{}")
+    
+    # Look for things to do
     if col in df.columns:
+        # If a file is to be substituted
+        # Throws an error if the file is not found
         if 'file=' in df[col][rowno]:
             print ('Inserting file', df[col][rowno][5:])
             with open(df[col][rowno][5:]) as fl:
                 temp = fl.read()
-            return re.sub(regexcurl,curl_replace,temp)
-        print (match, ' -- ', df[col][rowno])
+
+            # Recursively process the file
+            return re.sub(regexcurl,compile, re.sub(regexsquare, preprocess, temp))
+        
+        # Return the value of the requested cell
+        print (col, ' -- ', df[col][rowno])
+        
+        # Look for http links and replace special characters
+        # TODO: If necessary, replace special characters everywhere
         if df[col][rowno][:4] == 'http':
             print('Detected http link')
             return replace_special(df[col][rowno])
+        
         return df[col][rowno]
     else:
+        # No such column
         return match
     
+# Regex to be used
 regexsquare = r"\[(.*?)\]"
 regexcurl = r"{{([^}]+)}}"
 regexcaret = r"\^\^([^}]+)\^\^"
 regexdollar = r"\$\$(.*?)\$\$"
 regexnotdollar = r"\$!(.*?)!\$"
 
+# Main loop
 for index,row in df.iterrows():
+    # Update index for other functions
     rowno = index
-    filename = '../' + row['os_abbr'] + '/' + row['code_name'] + '.html'
+    
+    # Get the file to save to
+    filename = '../' + row['filename'] + '/' + row['code_name'] + '.html'
     print(' ------------ Now working on ', filename, '------------')
-    f = open(filename, 'w')
-    square_processed = re.sub(regexsquare,square_replace,lines)
-    f.write(re.sub(regexcurl,curl_replace,square_processed))
-    f.close()
+    
+    # Start working
+    final = re.sub(regexcurl, compile, re.sub(regexsquare, preprocess, template))
+    
+    # Write out everything when done
+    with open(filename, 'w') as f:
+        f.write(final)
+
 print('Everything done')
